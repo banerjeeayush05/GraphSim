d52a6972595db2fa836b7b714a5b7de313d48e5c
wehaveagod <banerjeayush05@gmail.com>
2022-11-17 15:56:45 -0500

    Finalized the user interface

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import math
 from module.edge import Edge
 from module.vertex import Vertex
 
+
+
 WIDTH = 600
 HEIGHT = 600
 
@@ -20,16 +22,25 @@ add_lines_rect = pygame.Rect(WIDTH/2-30, 0, 100, 30)
 vertices = []
 edges = []
 
+def check_in_vertex(vertices, x_coord, y_coord):
+    vertex_label = -1
+    for i in range(len(vertices)):
+        if(math.hypot(vertices[i].x - x_coord, vertices[i].y - y_coord) <= RADIUS):
+            vertex_label = vertices[i].label
+            break
+    return vertex_label
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("GRAPH THEORY SIMULATOR")
 
 pygame.font.init()
-font = pygame.font.SysFont('Comic Sans', 20)
+text_font = pygame.font.SysFont('Comic Sans', 20)
+label_font = pygame.font.SysFont('Comic Sans', 10)
 
-add_line_text = font.render('ADD LINE', False, WHITE)
-add_line_remove = font.render('ADD LINE', False, BLACK)
-clear_screen_text = font.render('CLEAR SCREEN', False, WHITE)
+add_line_text = text_font.render('ADD LINE', False, WHITE)
+add_line_remove_text = text_font.render('ADD LINE', False, BLACK)
+clear_screen_text = text_font.render('CLEAR SCREEN', False, WHITE)
 
 def main():
     running = True
@@ -42,6 +53,8 @@ def main():
     last_mouse_x = 0
     last_mouse_y = 0
 
+    label = 1
+
     pygame.draw.rect(screen, GRAY, clear_screen_rect)
     screen.blit(clear_screen_text, (5, HEIGHT-30))
 
@@ -52,11 +65,16 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 first_mouse_x = pygame.mouse.get_pos()[0]
                 first_mouse_y = pygame.mouse.get_pos()[1]
-                if(is_long_clicked and not two_points and any(math.hypot(pygame.mouse.get_pos()[0] - vertex.x, pygame.mouse.get_pos()[1] - vertex.y) <= RADIUS for vertex in vertices)):
+                if(is_long_clicked and not two_points and any(math.hypot(first_mouse_x - vertex.x, first_mouse_y - vertex.y) <= RADIUS for vertex in vertices)):
+                    for vertex in vertices:
+                        if (math.hypot(first_mouse_x - vertex.x, first_mouse_y - vertex.y) <= RADIUS):
+                            end_label = vertex.label
+                        if (math.hypot(last_mouse_x - vertex.x, last_mouse_y - vertex.y) <= RADIUS):
+                            start_label = vertex.label
                     pygame.draw.line(screen, WHITE, (last_mouse_x, last_mouse_y), (first_mouse_x, first_mouse_y), 3)
-                    edges.append(Edge(first_mouse_x, first_mouse_y, last_mouse_x, last_mouse_y))
+                    edges.append(Edge(first_mouse_x, first_mouse_y, last_mouse_x, last_mouse_y, start_label, end_label))
 
-                    screen.blit(add_line_remove, (WIDTH/2-30, 0))
+                    screen.blit(add_line_remove_text, (WIDTH/2-30, 0))
                     pygame.draw.rect(screen, BLACK, add_lines_rect)
 
                     two_points = True
@@ -77,6 +95,7 @@ def main():
                     is_long_clicked = False
                     two_points = True
                     clear_button_check = False
+                    label = 1
 
                     first_mouse_x = 0
                     first_mouse_y = 0
@@ -113,7 +132,10 @@ def main():
                                 break
                         if(ind and not is_long_clicked and last_mouse_x + RADIUS < WIDTH and last_mouse_x - RADIUS > 0 and last_mouse_y + RADIUS < HEIGHT and last_mouse_y - RADIUS > 0 and not pygame.Rect.collidepoint(clear_screen_rect, (last_mouse_x, last_mouse_y)) and not pygame.Rect.collidepoint(add_lines_rect, (last_mouse_x, last_mouse_y))):
                             pygame.draw.circle(screen, WHITE, (last_mouse_x, last_mouse_y), RADIUS)
-                            vertices.append(Vertex(last_mouse_x, last_mouse_y, 1))
+                            vertices.append(Vertex(last_mouse_x, last_mouse_y, label))
+                            label_text = label_font.render(str(label), False, WHITE)
+                            screen.blit(label_text, (last_mouse_x - 15, last_mouse_y - 7.5))
+                            label += 1
             pygame.display.update()
     pygame.quit()
 
